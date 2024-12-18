25e5ef75a89bd83108beb6f03c518601dc872bc3
Roigith <roigith@outlook.com>
2024-12-17 19:22:16 -0500

    PostDetailScreen update & Drop Down Menu

--- components/home/head/Header.js
+++ components/home/head/Header.js
@@ -1,59 +1,115 @@
-import React from "react";
-import { Text, View, StyleSheet, Image, Pressable } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { GlobalStyles } from "../../../constants/Styles";
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Image, Pressable, Modal, TouchableOpacity } from "react-native";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+import { GlobalStyles, DEFAULT_DP } from "../../../constants/Styles";
 import PressEffect from "../../UI/PressEffect";
 
-const Header = ({ navigation }) => (
-  <View style={[styles.container]}>
-    <Pressable
-      onPress={() => navigation.navigate("UserProfileScreen")}
-      style={{ position: "absolute", left: 0 }}
-    >
-      <PressEffect>
-        <Image
-          style={{ width: 30, height: 30, borderRadius: 50 }}
-          source={{
-            uri: "https://p16.tiktokcdn.com/tos-maliva-avt-0068/2f134ee6b5d3a1340aeb0337beb48f2d~c5_720x720.jpeg",
-          }}
-        />
-      </PressEffect>
-    </Pressable>
+const Header = ({ navigation }) => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [addMenuVisible, setAddMenuVisible] = useState(false);
 
-    <View style={{ alignItems: "center" }}>
-      <Text style={{ color: "white", fontSize: 30, fontWeight: "bold" }}>
-        Social
-      </Text>
-      <Text style={{ color: "rgba(255,255,255,0.5)", fontSize: 15 }}>
-        Welcome To Social
-      </Text>
-    </View>
+  const toggleMenu = () => setMenuVisible(!menuVisible);
+  const toggleAddMenu = () => setAddMenuVisible(!addMenuVisible);
 
-    <View style={styles.iconsContainer}>
-      <PressEffect>
-        <Pressable
-          style={styles.icon}
-          onPress={() => {
-            navigation.navigate("SearchScreen");
-          }}
-        >
-          <Ionicons name="person-add-outline" size={25} color={"white"} />
-        </Pressable>
-      </PressEffect>
-      <PressEffect>
-        <Pressable
-          style={styles.icon}
-          onPress={() => {
-            navigation.navigate("NotificationsScreen");
-          }}
-        >
-          <Ionicons name="notifications" size={25} color={"white"} />
-          <View style={styles.unreadBadge} />
-        </Pressable>
-      </PressEffect>
+  return (
+    <View style={[styles.container]}>
+      <Pressable onPress={toggleAddMenu} style={{ position: "absolute", left: 0 }}>
+        <PressEffect>
+          <Image
+            style={{ width: 30, height: 30, borderRadius: 50 }}
+            source={{ uri: "https://p16.tiktokcdn.com/tos-maliva-avt-0068/2f134ee6b5d3a1340aeb0337beb48f2d~c5_720x720.jpeg" }}
+          />
+        </PressEffect>
+      </Pressable>
+
+      <View style={{ alignItems: "center" }}>
+        <Text style={{ color: "white", fontSize: 30, fontWeight: "bold" }}>Social</Text>
+        <Text style={{ color: "rgba(255,255,255,0.5)", fontSize: 15 }}>Welcome To Social</Text>
+      </View>
+
+      <View style={styles.iconsContainer}>
+        <PressEffect>
+          <Pressable
+            style={styles.icon}
+            onPress={() => {
+              navigation.navigate("SearchScreen");
+            }}
+          >
+            <MaterialIcons name="person-add" size={25} color={"white"} />
+          </Pressable>
+        </PressEffect>
+        <PressEffect>
+          <Pressable
+            style={styles.icon}
+            onPress={() => {
+              navigation.navigate("NotificationsScreen");
+            }}
+          >
+            <MaterialIcons name="notifications" size={25} color={"white"} />
+            <View style={styles.unreadBadge} />
+          </Pressable>
+        </PressEffect>
+        <PressEffect>
+          <Pressable style={styles.icon} onPress={toggleMenu}>
+            <MaterialIcons name="account-circle" size={25} color={"white"} />
+          </Pressable>
+        </PressEffect>
+      </View>
+
+      {/* Dropdown Menu for Add Options */}
+      <Modal transparent={true} visible={addMenuVisible} animationType="fade">
+        <TouchableOpacity style={styles.overlay} onPress={toggleAddMenu}>
+          <View style={styles.dropdownMenu}>
+            <TouchableOpacity style={styles.menuItem} onPress={() => console.log("Add Activity")}>
+              <Ionicons name="add-circle-outline" size={24} color={GlobalStyles.colors.purple} />
+              <Text style={styles.menuText}>Add Activity</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuItem} onPress={() => console.log("Add Post")}>
+              <Ionicons name="add-circle-outline" size={24} color={GlobalStyles.colors.purple} />
+              <Text style={styles.menuText}>Add Post</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuItem} onPress={() => console.log("Add Story")}>
+              <Ionicons name="add-circle-outline" size={24} color={GlobalStyles.colors.purple} />
+              <Text style={styles.menuText}>Add Story</Text>
+            </TouchableOpacity>
+          </View>
+        </TouchableOpacity>
+      </Modal>
+
+      {/* Dropdown Menu for Account Options */}
+      <Modal transparent={true} visible={menuVisible} animationType="fade">
+        <TouchableOpacity style={styles.overlay} onPress={toggleMenu}>
+          <View style={styles.dropdownMenu}>
+            <TouchableOpacity style={styles.menuItem} onPress={() => console.log("My profile")}>
+              <MaterialIcons name="account-circle" size={24} color={GlobalStyles.colors.primary} />
+              <Text style={styles.menuText}>My profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuItem} onPress={() => console.log("Edit profile")}>
+              <MaterialIcons name="edit" size={24} color={GlobalStyles.colors.primary} />
+              <Text style={styles.menuText}>Edit profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuItem} onPress={() => console.log("Inbox")}>
+              <MaterialIcons name="inbox" size={24} color={GlobalStyles.colors.primary} />
+              <Text style={styles.menuText}>Inbox</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuItem} onPress={() => console.log("Settings")}>
+              <MaterialIcons name="settings" size={24} color={GlobalStyles.colors.primary} />
+              <Text style={styles.menuText}>Setting</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuItem} onPress={() => console.log("Help")}>
+              <MaterialIcons name="help" size={24} color={GlobalStyles.colors.primary} />
+              <Text style={styles.menuText}>Help</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuItem} onPress={() => console.log("Logout")}>
+              <MaterialIcons name="logout" size={24} color={GlobalStyles.colors.primary} />
+              <Text style={styles.menuText}>Logout</Text>
+            </TouchableOpacity>
+          </View>
+        </TouchableOpacity>
+      </Modal>
     </View>
-  </View>
-);
+  );
+};
 
 export default Header;
 
@@ -84,9 +140,38 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  unreadBadgeText: {
-    fontSize: 10,
-    color: "white",
-    fontWeight: "bold",
+  overlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.3)",
+  },
+  dropdownMenu: {
+    backgroundColor: GlobalStyles.colors.primary600, // Use the lighter color here
+    width: 200,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    padding: 10,
+    alignItems: "center",
+  },
+  menuItem: {
+    paddingVertical: 10,
+    width: "100%",
+    alignItems: "center",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  menuIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 10,
+  },
+  menuText: {
+    color: GlobalStyles.colors.purple,
+    fontSize: 16,
   },
 });
--- screens/PostDetailScreen.js
+++ screens/PostDetailScreen.js
@@ -4,11 +4,10 @@ import {
   Text,
   Image,
   StyleSheet,
-  ScrollView,
   Dimensions,
   ImageBackground,
   TouchableOpacity,
-  Pressable,
+  FlatList,
 } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
@@ -42,7 +41,7 @@ const PostDetailScreen = () => {
   };
 
   return (
-    <ScrollView style={styles.screen}>
+    <View style={styles.screen}>
       {/* Post Image */}
       <ImageBackground
         source={{ uri: post.picturePath }}
@@ -57,7 +56,7 @@ const PostDetailScreen = () => {
         />
         <View style={styles.headerInfo}>
           <View style={{ flexDirection: "row" }}>
-            <Pressable
+            <TouchableOpacity
               style={{ flexDirection: "row" }}
               onPress={() => {
                 navigation.navigate("UserProfileScreen", {
@@ -67,24 +66,14 @@ const PostDetailScreen = () => {
               }}
             >
               <Image
-                source={
-                  post.userPicturePath
-                    ? { uri: post.userPicturePath }
-                    : {
-                        uri: "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
-                      }
-                }
+                source={post.userPicturePath ? { uri: post.userPicturePath } : { uri: "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" }}
                 style={styles.userAvatar}
               />
               <View style={{ marginLeft: 10 }}>
-                <Text style={{ color: "white", fontWeight: "bold", fontSize: 15 }}>
-                  {post.username}
-                </Text>
-                <Text style={{ color: "rgba(255,255,255,0.3)", fontSize: 10, fontWeight: "bold" }}>
-                  {new Date(post.createdAt).toLocaleDateString("en-CA")}
-                </Text>
+                <Text style={{ color: "white", fontWeight: "bold", fontSize: 15 }}>{post.username}</Text>
+                <Text style={{ color: "rgba(255,255,255,0.3)", fontSize: 10, fontWeight: "bold" }}>{new Date(post.createdAt).toLocaleDateString("en-CA")}</Text>
               </View>
-            </Pressable>
+            </TouchableOpacity>
           </View>
         </View>
       </ImageBackground>
@@ -92,40 +81,24 @@ const PostDetailScreen = () => {
       {/* Post Content */}
       <View style={styles.content}>
         <Text style={styles.title}>{post.title}</Text>
-
         <View style={styles.tagsContainer}>
           {post.tags.map((tag, index) => (
-            <Text key={index} style={styles.tag}>
-              {tag}
-            </Text>
+            <Text key={index} style={styles.tag}>{tag}</Text>
           ))}
         </View>
-
         <Text style={styles.price}>${post.prices.amount}</Text>
-
         <View style={styles.locationContainer}>
-          <Ionicons name="location" size={15} color={GlobalStyles.colors.gray} />
-          <Text style={styles.location}>{post.location}</Text>
+          <Ionicons name="location" size={15} color={GlobalStyles.colors.magenta} />
+          <Text style={styles.location}>{post.location ? post.location : "Location not available"}</Text>
         </View>
-
         <Text style={styles.description}>{post.description}</Text>
       </View>
 
       {/* Post Stats */}
       <View style={styles.statsContainer}>
-        <Ionicons
-          name={isFavorite ? "heart" : "heart-outline"}
-          size={24}
-          color={GlobalStyles.colors.greenLight}
-          style={styles.icon}
-        />
+        <Ionicons name={isFavorite ? "heart" : "heart-outline"} size={24} color={GlobalStyles.colors.greenLight} style={styles.icon} />
         <Text style={styles.statsText}>{isFavorite ? "Added to favorites" : `${post.likes.length} likes`}</Text>
-        <Ionicons
-          name="chatbubble-ellipses"
-          size={24}
-          color={GlobalStyles.colors.blue}
-          style={styles.icon}
-        />
+        <Ionicons name="chatbubble-ellipses" size={24} color={GlobalStyles.colors.blue} style={styles.icon} />
         <Text style={styles.statsText}>{post.comments.length} comments</Text>
       </View>
 
@@ -139,27 +112,42 @@ const PostDetailScreen = () => {
       <View style={styles.socialMediaContainer}>
         <Text style={styles.socialMediaHeader}>Find us on:</Text>
         <View style={styles.socialMediaButtons}>
-          <TouchableOpacity
-            style={styles.socialButton}
-            onPress={() => openSocialMedia("facebook")}
-          >
+          <TouchableOpacity style={styles.socialButton} onPress={() => openSocialMedia("facebook")}>
             <Ionicons name="logo-facebook" size={24} color="white" />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.socialButton}
-            onPress={() => openSocialMedia("instagram")}
-          >
+          <TouchableOpacity style={styles.socialButton} onPress={() => openSocialMedia("instagram")}>
             <Ionicons name="logo-instagram" size={24} color="white" />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.socialButton}
-            onPress={() => openSocialMedia("twitter")}
-          >
+          <TouchableOpacity style={styles.socialButton} onPress={() => openSocialMedia("twitter")}>
             <Ionicons name="logo-twitter" size={24} color="white" />
           </TouchableOpacity>
         </View>
       </View>
-    </ScrollView>
+
+      {/* Images Below Social Media Links */}
+      <Text style={styles.seeMoreText}>See more of us</Text>
+      <FlatList
+        data={[
+          { id: "1", imageUrl: "https://www.lehighvalleygrandprix.com/wp-content/uploads/2018/02/Go_Kart_Speed-1.jpg" },
+          { id: "2", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/9/94/2013_Australian_Open_-_Guillaume_Rufin.jpg" },
+          { id: "3", imageUrl: "https://www.kunstloft.com/wordpress/en_UK/eu/wp-content/uploads/2023/07/Painter-with-landscape-painting.jpg" },
+          { id: "4", imageUrl: "https://visitorinvictoria.ca/wp-content/uploads/2016/09/ranurte-a-CnhYgTenY-unsplash.jpg" },
+          { id: "5", imageUrl: "https://www.villevillemarie.org/wp-content/uploads/2023/06/Parc-des-Clubs_01-scaled.jpeg" },
+          { id: "6", imageUrl: "https://www.capdagde.com/app/uploads/2022/10/AdobeStock-discotheque-1198x800.jpeg" },
+          { id: "7", imageUrl: "https://bigkahunas.com/destin/wp-content/uploads/sites/11/2023/11/Big-Kahunas-Destin-Water-Park-226.jpg" },
+          { id: "8", imageUrl: "https://www.ivazio.com/wp-content/uploads/2022/08/karaoke-3-chanteurs-1-2560x1920.jpg" },
+          { id: "9", imageUrl: "https://www.levelupreality.ca/wp-content/uploads/2024/09/Couple-Playing-Escape-Simulator.webp" },
+        ]}
+        keyExtractor={item => item.id}
+        numColumns={3}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image source={{ uri: item.imageUrl }} style={styles.image} />
+          </View>
+        )}
+        contentContainerStyle={{ padding: 10 }}
+      />
+    </View>
   );
 };
 
@@ -182,106 +170,119 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   headerInfo: {
-    padding: 10,
+    padding: 20,
     flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
   },
   userAvatar: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    marginRight: 10,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
   },
   content: {
     padding: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: "bold",
-    color: GlobalStyles.colors.purple,
     marginBottom: 10,
+    color: "white"
   },
   tagsContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
-    marginVertical: 10,
-    gap: 5,
+    marginBottom: 10,
   },
   tag: {
     backgroundColor: GlobalStyles.colors.blue,
-    color: "white",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    borderRadius: 10,
+    borderRadius: 5,
+    margin: 5,
     fontSize: 12,
-    fontWeight: "bold",
+    color: "white",
   },
   price: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: "bold",
-    color: GlobalStyles.colors.greenLight,
-    marginVertical: 10,
+    color: GlobalStyles.colors.green,
+    marginBottom: 10,
   },
   locationContainer: {
     flexDirection: "row",
     alignItems: "center",
-    marginVertical: 10,
+    marginBottom: 10,
   },
   location: {
-    color: GlobalStyles.colors.gray,
     marginLeft: 5,
+    fontSize: 14,
+    color: "rgba(255,255,255,0.6)",
   },
   description: {
     fontSize: 16,
     color: "white",
-    marginTop: 10,
-    lineHeight: 24,
   },
   statsContainer: {
     flexDirection: "row",
+    justifyContent: "space-around",
     alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 20,
-  },
-  icon: {
-    marginHorizontal: 10,
+    backgroundColor: GlobalStyles.colors.primary200,
+    padding: 10,
   },
   statsText: {
+    fontSize: 12,
     color: "white",
-    fontSize: 16,
+    marginLeft: 5,
   },
   saveButton: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: GlobalStyles.colors.purple,
-    margin: 20,
+    backgroundColor: GlobalStyles.colors.blue,
     padding: 10,
-    borderRadius: 10,
+    marginVertical: 10,
+    justifyContent: "center",
   },
   saveButtonText: {
     color: "white",
-    marginLeft: 10,
-    fontSize: 16,
-    fontWeight: "bold",
+    marginLeft: 5,
   },
   socialMediaContainer: {
     padding: 20,
-    alignItems: "center",
+    backgroundColor: GlobalStyles.colors.primary300,
+    marginVertical: 10,
   },
   socialMediaHeader: {
     fontSize: 18,
+    fontWeight: "bold",
     color: "white",
     marginBottom: 10,
   },
   socialMediaButtons: {
     flexDirection: "row",
-    justifyContent: "center",
-    gap: 10,
+    justifyContent: "space-around",
   },
   socialButton: {
     backgroundColor: GlobalStyles.colors.blue,
+    borderRadius: 25,
     padding: 10,
-    borderRadius: 50,
+  },
+  card: {
+    flex: 1,
+    margin: 5,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: 100,
+    resizeMode: "cover",
+  },
+  seeMoreText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: GlobalStyles.colors.purple,
+    textAlign: "center",
+    marginVertical: 10,
   },
 });
